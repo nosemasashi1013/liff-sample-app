26de6de3db254577c6969233b7e7eea3dfd19432
nosemasashi1013 <nose_masashi@cyberagent.co.jp>
2021-10-06 12:55:57 +0900

    feat: first commit

---
+++ liff.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
+    // LINE DevelopersのLIFF画面より確認可能
+    var liffId = "(自分のLIFFIDを入力)";
+    initializeLiff(liffId);
+})
+
+function initializeLiff(liffId) {
+    liff
+        .init({
+            liffId: liffId
+        })
+        .then(() => {
+            // Webブラウザからアクセスされた場合は、LINEにログインする
+            if (!liff.isInClient() && !liff.isLoggedIn()) {
+                window.alert("LINEアカウントにログインしてください。");
+                liff.login({redirectUri: location.href});
+            }
+        })
+        .catch((err) => {
+            console.log('LIFF Initialization failed ', err);
+        });
+}
+
+// LINEトーク画面上でメッセージ送信
+function sendMessages(text) {
+    liff.sendMessages([{
+        'type': 'text',
+        'text': text
+    }]).then(function () {
+        liff.closeWindow();
+    }).catch(function (error) {
+        window.alert('Failed to send message ' + error);
+    });
+}
