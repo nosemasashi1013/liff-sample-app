5ce6c62fab420f669404a80792d3c1f778868dff
nosemasashi1013 <nose_masashi@cyberagent.co.jp>
2021-10-06 19:05:17 +0900

    fix

--- liff.js
+++ liff.js
@@ -76,7 +76,7 @@ $(function () {
   initializeLiff(liffId);
 
   $(".open-camera-btn").click(function () {
-    scanCodeV2();
+    scanCode();
   });
 
   $(".submit-btn").click(function () {
@@ -226,3 +226,44 @@ $(function () {
 //       });
 //   }
 // });
+
+// $(document).ready(function () {
+//   // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
+//   // LINE DevelopersのLIFF画面より確認可能
+//   var liffId = "1656501658-pq7GAjnA";
+//   initializeLiff(liffId);
+// });
+
+// function initializeLiff(liffId) {
+//   liff
+//     .init({
+//       liffId: liffId,
+//     })
+//     .then(() => {
+//       // Webブラウザからアクセスされた場合は、LINEにログインする
+//       if (!liff.isInClient() && !liff.isLoggedIn()) {
+//         window.alert("LINEアカウントにログインしてください。");
+//         liff.login({ redirectUri: location.href });
+//       }
+//     })
+//     .catch((err) => {
+//       console.log("LIFF Initialization failed ", err);
+//     });
+// }
+
+// // LINEトーク画面上でメッセージ送信
+// function sendMessages(text) {
+//   liff
+//     .sendMessages([
+//       {
+//         type: "text",
+//         text: text,
+//       },
+//     ])
+//     .then(function () {
+//       liff.closeWindow();
+//     })
+//     .catch(function (error) {
+//       window.alert("Failed to send message " + error);
+//     });
+// }
